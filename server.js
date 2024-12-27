90c671265704fdeba724427e35127251106cba88
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2024-12-27 22:49:43 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const rooms = new Map();
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        
+        switch(data.type) {
+            case 'join':
+                const room = rooms.get(data.room) || new Set();
+                room.add(ws);
+                rooms.set(data.room, room);
+                break;
+                
+            case 'offer':
+            case 'answer':
+            case 'ice-candidate':
+                const targetRoom = rooms.get(data.room);
+                if (targetRoom) {
+                    targetRoom.forEach(client => {
+                        if (client !== ws) {
+                            client.send(JSON.stringify(data));
+                        }
+                    });
+                }
+                break;
+        }
+    });
+});
+
+server.listen(8000, () => {
+    console.log('Server running on port 8000');
+});
